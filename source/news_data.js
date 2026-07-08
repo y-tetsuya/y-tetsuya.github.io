09a84f8c08ec98ce89a37d069ef9fc860d8b078a
Tetsuya Yasuda <111479005+y-tetsuya@users.noreply.github.com>
2026-07-08 17:37:14 +0900

    Add files via upload
    updating

--- source/news_data.js
+++ source/news_data.js
@@ -3,6 +3,12 @@ window.newsContentMap = {
     "title": "ニュース",
     "items": [
       {
+        "date": "2026/5/7",
+        "title": "懇親会",
+        "description": "安田研の皆さんとRickyで懇親会をしました。",
+        "link": "https://www.instagram.com/ricky_yakitori.robata/"
+      },
+      {
         "date": "2026/4/9",
         "title": "新年度スタート",
         "description": "2026年度の研究活動を開始しました！",
@@ -31,6 +37,18 @@ window.newsContentMap = {
   "publication-news": {
     "title": "発表・論文についてのお知らせ",
     "items": [
+      {
+        "date": "2026/6/10",
+        "title": "学会発表予定(共同研究)",
+        "description": "東京電機大学 小林春美教授との共同研究を日本認知科学会第43回大会で発表することが決まりました。",
+        "link": "https://pub.confit.atlas.jp/ja/event/jcss2026"
+      },
+      {
+        "date": "2026/5/20",
+        "title": "学会発表予定(共同研究)",
+        "description": "東京電機大学 小林春美教授との共同研究をJSLS2026で発表することが決まりました。",
+        "link": "https://jslsweb.sakura.ne.jp/wp/en/1691-2/jsls-2026/call-for-papers/"
+      },
       {
         "date": "2026/3/4",
         "title": "学会発表(共同研究)",
